91b4393f98828c82098716cd59b72791fc43da3f
Neale2 <arturo.neale@gmail.com>
2025-08-15 21:57:45 +1200

    Animation cleaning up :)

--- lure/lib/hero_dialog_route.dart
+++ lure/lib/hero_dialog_route.dart
@@ -16,7 +16,7 @@ class HeroDialogRoute<T> extends PageRoute<T> {
   bool get barrierDismissible => true;
 
   @override
-  Duration get transitionDuration => const Duration(milliseconds: 300);
+  Duration get transitionDuration => const Duration(milliseconds: 350); // Slightly longer for a smoother feel
 
   @override
   bool get maintainState => true;
@@ -33,14 +33,20 @@ class HeroDialogRoute<T> extends PageRoute<T> {
     return _builder(context);
   }
 
+  // --- THIS IS THE UPDATED METHOD ---
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation,
       Animation<double> secondaryAnimation, Widget child) {
     return BackdropFilter(
       filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+      // Combine a fade and a scale transition for a smooth "pop-up" effect.
       child: FadeTransition(
         opacity: CurvedAnimation(parent: animation, curve: Curves.easeOut),
-        child: child,
+        child: ScaleTransition(
+          // The scale animation is driven by the main animation controller.
+          scale: CurvedAnimation(parent: animation, curve: Curves.easeOutCubic),
+          child: child,
+        ),
       ),
     );
   }
